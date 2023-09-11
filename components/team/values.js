fc6b40e5f45f9b867c8d7d8904400f9ffcb2216a
unknown <hernandez.jrme@gmail.com>
2023-09-11 17:04:20 +0200

    Modification avec LAurent

--- components/team/values.js
+++ components/team/values.js
@@ -4,8 +4,8 @@ import Cta from "../cta";
 function Values() {
   return (
     <>
-      <div className=" py-16 w-full lg:py-20 ">
-        <div className="grid grid-cols-2 row-gap-8 md:grid-cols-5 text-blue-700">
+      <div className="pb-6 w-full  ">
+        <div className="grid grid-cols-2 row-gap-3 md:grid-cols-5 text-blue-700">
           <div className="text-center md:border-r">
             <h6 className="text-4xl font-bold lg:text-4xl xl:text-4xl">
               Ecoute
