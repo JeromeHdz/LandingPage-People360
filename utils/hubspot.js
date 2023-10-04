9455cb0c8f39dd3b5008a87b14e3b614753dacde
JeromeHDZ <hernandez.jrme@gmail.com>
2023-10-04 15:35:40 +0200

    Adding hubspot

--- utils/hubspot.js
+++ utils/hubspot.js
@@ -3,9 +3,7 @@ import React from 'react';
 const Hubspot = () => {
   return (
     <>
-      {/* <!-- Start of HubSpot Embed Code --> */}
-        <script type="text/javascript" id="hs-script-loader" async defer src="//js-eu1.hs-scripts.com/143410325.js"></script>
-    {/* <!-- End of HubSpot Embed Code --> */}
+    <script type="text/javascript" id="hs-script-loader" async defer src="//js-eu1.hs-scripts.com/143410325.js"></script>
       <script
         dangerouslySetInnerHTML={{
           __html: `
