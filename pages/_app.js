5f931b6898ebca87dd70eb8e3cc74140b0fe4edf
JeromeHDZ <hernandez.jrme@gmail.com>
2023-10-04 15:41:30 +0200

    Adding hubspot in _document.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,10 @@
 import { ThemeProvider } from "next-themes";
 import "../css/tailwind.css";
-import Hubspot from "../utils/hubspot";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
-      <Hubspot />
     </ThemeProvider>
   );
 }
